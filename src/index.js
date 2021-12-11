bdc0721a146748e9e6aa3d3edaebf03d9e88dbdf
dcc8233299 <cdan199221@gmail.com>
2021-12-11 23:02:19 +1100

    add assets management for loading data

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 import './style.css';
 import Icon from './digimon.png';
+import Data from './data.xml';
+import Notes from './data.csv';
+// Warning: Should not import the named export 'data' (imported as 'data') from default-exporting module (only default export is available soon)
+import { data } from './data.json';
+import Toml from './data.toml';
+import Yaml from './data.yaml';
+import Json5 from './data.json5';
+
+console.log(Toml.title); // output `TOML Example`
+console.log(Toml.owner.name); // output `Tom Preston-Werner`
+
+console.log(Yaml.title); // output `YAML Example`
+console.log(Yaml.owner.name); // output `Tom Preston-Werner`
+
+console.log(Json5.title); // output `JSON5 Example`
+console.log(Json5.owner.name); // output `Tom Preston-Werner`
 
 function component() {
   const element = document.createElement('div');
@@ -11,6 +27,11 @@ function component() {
   myIcon.src = Icon;
 
   element.appendChild(myIcon);
+
+  console.log(Data);
+  console.log(Notes);
+  console.log(data);
+
   return element;
 }
 
