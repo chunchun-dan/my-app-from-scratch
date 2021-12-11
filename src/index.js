4f0abaab85c0f6523fd5e35586d1df70806f0d21
dcc8233299 <cdan199221@gmail.com>
2021-12-11 16:47:06 +1100

    add assets management for image

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './style.css';
+import Icon from './digimon.png';
 
 function component() {
   const element = document.createElement('div');
@@ -6,6 +7,10 @@ function component() {
   element.innerHTML = "Hello World!";
   element.classList.add('hello');
 
+  const myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
   return element;
 }
 
