efb78528d42cfcfe882cae59a51559d48c7a0b7b
dcc8233299 <cdan199221@gmail.com>
2021-12-12 23:31:50 +1100

    use html-webpack-plugin to generate sbundles dynamically

--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,22 @@ const path = require('path');
 const toml = require('toml');
 const yaml = require('yamljs');
 const json5 = require('json5');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
     index: './src/index.js',
     print: './src/print.js',
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+    }),
+  ],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
   module: {
     rules: [
