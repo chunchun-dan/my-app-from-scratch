6bb078239008346e675cbd4d9ba0f9c759ecf0e3
dcc8233299 <cdan199221@gmail.com>
2021-12-19 23:29:50 +1100

    create a clock component

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Clock from './App/Clock';
 
 const domContainer = document.getElementById('root');
 
 if (domContainer) {
   ReactDOM.render(
-    <App />,
+    <Clock />,
     domContainer
   );
 }
