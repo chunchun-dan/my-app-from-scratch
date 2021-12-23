1f3438e693872c96aee029fc6d8da8550b30a13a
dcc8233299 <cdan199221@gmail.com>
2021-12-24 09:08:01 +1100

    add input and multiple select

--- src/App/NameForm.js
+++ src/App/NameForm.js
@@ -4,13 +4,23 @@ class NameForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      inputValue: '',
       textValue: 'Please type in text here',
       selectValue: 'coconut',
+      multiValue: ['a', 'b']
     };
 
+    this.handleInputChange = this.handleInputChange.bind(this);
     this.handleTextChange = this.handleTextChange.bind(this);
     this.handleSelectChange = this.handleSelectChange.bind(this);
+    this.handleMultiChange = this.handleMultiChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleInputChange(e) {
+    this.setState({
+      inputValue: e.target.value
+    });
   }
 
   handleTextChange(e) {
@@ -25,10 +35,27 @@ class NameForm extends React.Component {
     });
   }
 
+  handleMultiChange(e) {
+    if (this.state.multiValue.indexOf(e.target.value) > -1) {
+      const index = this.state.multiValue.findIndex((v) => v === e.target.value)
+      let multi = this.state.multiValue;
+      multi.splice(index, 1);
+      this.setState({
+        multiValue: multi
+      })
+    }else{
+      this.setState({
+        multiValue: [...this.state.multiValue, e.target.value]
+      });
+    }
+  }
+
   handleSubmit(e) {
     e.preventDefault();
+    console.log('The input name is ' + this.state.inputValue);
     console.log('Some text is added as ' + this.state.textValue);
     console.log('Your favorite flavor is ' + this.state.selectValue);
+    console.log('Your multiple choice is ' + this.state.multiValue);
   }
 
   render() {
@@ -36,6 +63,10 @@ class NameForm extends React.Component {
       <form onSubmit={this.handleSubmit}>
         <div>
           {'Name: '}
+          <input value={this.state.inputValue} onChange={this.handleInputChange}></input>
+        </div>
+        <div>
+          {'Text: '}
           <textarea
             value={this.state.textValue}
             onChange={this.handleTextChange}
@@ -48,6 +79,14 @@ class NameForm extends React.Component {
             <option value="coconut">Coconut</option>
             <option value="Mango">Mango</option>
             <option value="Lime">Lime</option>
+          </select>
+        </div>
+        <div>
+          <select multiple={true} value={this.state.multiValue} onChange={this.handleMultiChange}>
+            <option value="a">A</option>
+            <option value="b">B</option>
+            <option value="c">C</option>
+            <option value="d">D</option>
           </select>
         </div>
         <input type="submit" value="Submit"/>
