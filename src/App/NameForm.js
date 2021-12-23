43b12ce84d892685678244d2768228456be66267
dcc8233299 <cdan199221@gmail.com>
2021-12-24 00:37:18 +1100

    add select element

--- src/App/NameForm.js
+++ src/App/NameForm.js
@@ -3,33 +3,53 @@ import React from 'react';
 class NameForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {value: 'Please type in text here'};
+    this.state = {
+      textValue: 'Please type in text here',
+      selectValue: 'coconut',
+    };
 
-    this.handleChange = this.handleChange.bind(this);
+    this.handleTextChange = this.handleTextChange.bind(this);
+    this.handleSelectChange = this.handleSelectChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleChange(e) {
+  handleTextChange(e) {
     this.setState({
-      value: e.target.value,
+      textValue: e.target.value,
+    });
+  }
+
+  handleSelectChange(e) {
+    this.setState({
+      selectValue: e.target.value,
     });
   }
 
   handleSubmit(e) {
     e.preventDefault();
-    console.log('Some text is added as ' + this.state.value);
+    console.log('Some text is added as ' + this.state.textValue);
+    console.log('Your favorite flavor is ' + this.state.selectValue);
   }
 
   render() {
     return(
       <form onSubmit={this.handleSubmit}>
-        <label>
+        <div>
           {'Name: '}
           <textarea
-            value={this.state.value}
-            onChange={this.handleChange}
+            value={this.state.textValue}
+            onChange={this.handleTextChange}
           />
-        </label>
+        </div>
+        <div>
+          {'Select your favorite flavor: '}
+          <select value={this.state.selectValue} onChange={this.handleSelectChange}>
+            <option value="grapefruit">Grapefruit</option>
+            <option value="coconut">Coconut</option>
+            <option value="Mango">Mango</option>
+            <option value="Lime">Lime</option>
+          </select>
+        </div>
         <input type="submit" value="Submit"/>
       </form>
     );
